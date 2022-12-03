d32b8cc3b88f3b0a52400f4c0c677b40f6e476b4
Nathan Touchton <ntouchton@protonmail.com>
2022-12-03 12:38:58 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from smtplib import SMTP
+from datetime import datetime
+from random import choice
 
 GMAIL = "ntouchtongarbage@gmail.com"
 YAHOO = "ntouchtongarbage@yahoo.com"
 
-with SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=GMAIL, password="tritxyfgglkzhmwv")
-    connection.sendmail(
-        from_addr=GMAIL,
-        to_addrs=YAHOO,
-        msg="Subject:Hello\n\nThis is the body."
-    )
+NOW = datetime.now()
+with open("quotes.txt", mode="r") as file:
+    lines = file.readlines()
+    QUOTE = choice(lines)
+
+if NOW.weekday() == 5:
+    with SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(
+            user=GMAIL,
+            password="tritxyfgglkzhmwv"
+        )
+        connection.sendmail(
+            from_addr=GMAIL,
+            to_addrs=YAHOO,
+            msg=f"Subject:Motivational quote of the week\n\n{QUOTE}"
+        )

5bb892ba45aeb15e7ba3bf3ac3da455869e22449
Nathan Touchton <ntouchton@protonmail.com>
2022-12-05 23:02:39 -0600

    modified:   birthdays.csv 	modified:   main.py 	modified:   quotes.txt

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from random import randint
 from pandas import read_csv
 
-GMAIL = "ntouchtongarbage@gmail.com"
-YAHOO = "ntouchtongarbage@yahoo.com"
+GMAIL = "youremailherem"
+YAHOO = "youremailhere"
 
 # 1. Update the birthdays.csv
 
@@ -12,15 +12,16 @@ YAHOO = "ntouchtongarbage@yahoo.com"
 
 BIRTHDAY_LIST = read_csv("birthdays.csv")
 
-print(BIRTHDAY_LIST["year"][0])
+# print(BIRTHDAY_LIST["year"][0])
 
 IS_BIRTHDAY = True
 
 NOW = datetime.now()
 
-VALID_YEAR = BIRTHDAY_LIST[BIRTHDAY_LIST.year == NOW.year]
-VALID_MONTH = VALID_YEAR[BIRTHDAY_LIST.month == NOW.month]
+VALID_MONTH = BIRTHDAY_LIST[BIRTHDAY_LIST.month == NOW.month]
 VALID_DAY = VALID_MONTH[BIRTHDAY_LIST.day == NOW.day]
+if len(VALID_DAY) == 0:
+    IS_BIRTHDAY = False
 
 # 3. If step 2 is true, pick a random letter from letter templates
     # and replace the [NAME] with the person's actual name from birthdays.csv
@@ -28,26 +29,21 @@ VALID_DAY = VALID_MONTH[BIRTHDAY_LIST.day == NOW.day]
 LETTER_NUMBER = randint(1, 3)
 with open(f"letter_templates/letter_{LETTER_NUMBER}.txt", mode="r") as file:
     letter = file.read()
-    letter = letter.replace("[NAME]", "Bleh")
-    # Next time I need to write the code to replace the name with the name from the CSV.
-    print(letter)
+    new_name = VALID_DAY.name
+    new_name = new_name.to_string(index=False)
+    letter = letter.replace("[NAME]", new_name)
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
-# NOW = datetime.now()
-# with open("quotes.txt", mode="r") as file:
-#     lines = file.readlines()
-#     QUOTE = choice(lines)
-
-# if NOW.weekday() == 5:
-#     with SMTP("smtp.gmail.com", port=587) as connection:
-#         connection.starttls()
-#         connection.login(
-#             user=GMAIL,
-#             password="tritxyfgglkzhmwv"
-#         )
-#         connection.sendmail(
-#             from_addr=GMAIL,
-#             to_addrs=YAHOO,
-#             msg=f"Subject:Motivational quote of the week\n\n{QUOTE}"
-#         )
+if IS_BIRTHDAY:
+    with SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(
+            user=GMAIL,
+            password="Put password here"
+        )
+        connection.sendmail(
+            from_addr=GMAIL,
+            to_addrs=YAHOO,
+            msg=f"Subject:Happy Birthday!\n\n{letter}"
+        )
